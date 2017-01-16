e1a86dc50711c663d4e590fb8f991250a93397f8
UMAR <umar6545@gmail.com>
2017-01-16 10:51:34 +0500

    This is lab Final

--- calculator/src/test/java/pk/ciit/sc/Final1/calculator/CalculatorTest.java
+++ calculator/src/test/java/pk/ciit/sc/Final1/calculator/CalculatorTest.java
@@ -2,6 +2,7 @@ package pk.ciit.sc.Final1.calculator;
 
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class CalculatorTest {
@@ -13,11 +14,27 @@ public class CalculatorTest {
 		assertEquals(7, res);
 		
 	}
+	/**
+	 * this method could not find max values when all values are less than 0(<0)
+	 */
+	@Test
+	public void testMaxFailtoFind() {
+		int res=cal.findMax(-1, -17, -16);
+		assertEquals(-1, res);
+		
+	}
 
 	@Test
 	public void testSquare() {
 		int res=cal.square(6);
 		assertEquals(36, res);
+		
+	}
+	
+	@Ignore
+	@Test (expected = IllegalArgumentException .class)
+	public void testDevideException() throws IllegalArgumentException {
+		cal.square(15);
 		
 	}
 
